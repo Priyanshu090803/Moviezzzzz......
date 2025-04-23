1d0bd4f8dbbf728e88fcf13c342b44a825194376
Priyanshu090803 <priyanshu122a@gmail.com>
2025-04-23 16:39:07 +0530

    build: signin,signup page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className=' absolute w-full px-8 py-3 bg-gradient-to-b from-black'>
-      <img className=' w-20 sm:w-32 md:w-40 ' src="src/images/logo.png" alt='logo'/>
+    <div className=' z-10 absolute w-full px-20 py-3 bg-gradient-to-b from-black'>
+      <img className=' w-20 sm:w-32 md:w-36  ml-20 mt-4 ' src="src/images/logo.png" alt='logo'/>
     </div>
   )
 }
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,13 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './Header'
 
 const Login = () => {
+  const[isSignIn, setisSignIn] = useState(true);       // usestate se toggle karwa denge signin se sign up tak
+  const authtoggle=()=>{ 
+    setisSignIn(!isSignIn)                // for toggle
+  }
   return (
     <div>
       <Header/>
-      <div>
+      <div className=' relative'>
         <img src='https://assets.nflxext.com/ffe/siteui/vlv3/69bec183-9cc8-49d4-8fc2-08228d3c91b4/web/IN-en-20250414-TRIFECTA-perspective_c8273fb1-8860-4ff5-bd1c-c2c4b44d5f2a_large.jpg' alt='login-img'/>
+      
+       <form
+        className=' absolute w-[450px]  min-h-fit top-28 rounded-lg   left-3/8 gap-6 text-white flex flex-col px-16 py-14 bg-black/80	'>
+        <h1 className=' font-semibold  text-4xl mb-6'>{isSignIn?"Sign In":"Sign up"}</h1> 
+       
+        {!isSignIn&&(<input type='text'                               // logic for sign in
+        placeholder='First Name'
+        className=' text-gray-400 backdrop-blur font-semibold  border border-white h-14 rounded-md px-5'
+        />)}
+
+        {!isSignIn&&(<input type='text'
+        placeholder='Last Name'
+        className=' text-gray-400 backdrop-blur font-semibold  border border-white h-14 rounded-md px-5'
+        />)}
+
+       <input type='text'
+        placeholder='Email or mobile number'
+        className=' text-gray-400 backdrop-blur font-semibold  border border-white h-14 rounded-md px-5'
+        />
+        <input type='password'
+        placeholder='Password'
+        className=' text-gray-400  bg-opacity-70 backdrop-blur	 font-semibold  border border-white h-14 rounded-md px-5'
+
+        />
+        <button className=' text-white  font-semibold   h-10 bg-red-700 hover:bg-red-800 cursor-pointer rounded-md px-5 mt-10 mb-4' >{isSignIn?"Sign In":"Sign Up"}</button>
+        <p className=' cursor-pointer hover:underline hover:text-blue-400'
+        onClick={authtoggle}
+        >
+         {isSignIn?" New to Netflix? SignUp now": "Already a user? Sign In "}</p>
+      </form>
       </div>
+      
     </div>
   )
 }
